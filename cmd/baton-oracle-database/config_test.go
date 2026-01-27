7be029ec68be835c2a878e036b3722f199dd0e32
Steve Gontzes <gontzess@gmail.com>
2026-01-27 15:24:29 -0500

    Upgrade baton-sdk to v0.7.4 and Go to 1.25.x
    - Update baton-sdk dependency to v0.7.4
    - Update Go version to 1.25.2
    - Update field.Configuration API
    - Add WithDefaultCapabilitiesConnectorBuilder option
    - Update CI workflow to use go-version-file
    - Update golangci-lint-action to v8
    - Update .golangci.yml to v2 format
    - Add tools/rules.go for ruleguard

--- cmd/baton-oracle-database/config_test.go
+++ cmd/baton-oracle-database/config_test.go
@@ -5,17 +5,20 @@ import (
 
 	"github.com/conductorone/baton-sdk/pkg/field"
 	"github.com/conductorone/baton-sdk/pkg/test"
+	"github.com/spf13/viper"
 )
 
 func TestConfigs(t *testing.T) {
-	configurationSchema := field.NewConfiguration(
-		ConfigurationFields,
-		FieldRelationships...,
-	)
+	configurationSchema := field.Configuration{
+		Fields:      ConfigurationFields,
+		Constraints: FieldRelationships,
+	}
 
 	testCases := []test.TestCase{
 		// Add test cases here.
 	}
 
-	test.ExerciseTestCases(t, configurationSchema, ValidateConfig, testCases)
+	test.ExerciseTestCases(t, configurationSchema, func(v *viper.Viper) error {
+		return nil
+	}, testCases)
 }
